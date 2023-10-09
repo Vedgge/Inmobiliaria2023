d8ba64154bea90cb6bd059b20bb3b6dcc603d1f7
Facundo <facundosavanco@gmail.com>
2023-10-09 15:18:23 -0300

    Animación hamburguesa, agregado form login y singup

--- assets/script.js
+++ assets/script.js
@@ -11,14 +11,12 @@ mostrarPopup.addEventListener("click", function(){
 
 document.querySelector(".login .cerrar_btn").addEventListener("click", function(){
   popupLogin.classList.add("closing");
-  popupSignup.classList.add("closing");
   overlay.classList.add("closing");
+
   setTimeout(function() {
     popupLogin.classList.remove("active");
-    popupSignup.classList.remove("active");
     overlay.classList.remove("active"); //Elimina la clase active
     popupLogin.classList.remove("closing");
-    popupSignup.classList.remove("closing");
     overlay.classList.remove("closing"); //Elimina la clase de cierre después de la animación
   }, 500); 
 });
@@ -71,26 +69,26 @@ function sleep(ms){ //Funcion para esperar unos milisegundos
 const frases = ['hogar','departamento', 'local'];
 const elementos = document.getElementById("escritura");
 
-let sleepTime = 100; //Tiempo para esperar entre letras
+let waitTime = 100; //Tiempo para esperar entre letras
 
 let curFrases = 0; //Index por la frase que queremos empezar, vamos a incrementarla con lógica
 
-const loopEscritura = async () => { //Función async para llamar a la función sleepTime
+const loopEscritura = async () => { //Función async para llamar a la función waitTime
   while(true){ //loop para escribir las frases letra x letra
     let curLetra = frases[curFrases];
     for (let i = 0; i < curLetra.length; i++){
       elementos.innerText = curLetra.substring(0, i + 1);
-      await sleep(sleepTime);
+      await sleep(waitTime);
     }
 
-    await sleep(sleepTime * 10); //Tiempo de espera entre escritura y eliminación
+    await sleep(waitTime * 10); //Tiempo de espera entre escritura y eliminación
 
     for (let i = curLetra.length; i > 0; i--){ //Borrar letra x letra
       elementos.innerText = curLetra.substring(0, i - 1);
-      await sleep(sleepTime);
+      await sleep(waitTime);
     }
 
-    await sleep(sleepTime * 5); //Tiempo de espera entre eliminación y escritura
+    await sleep(waitTime * 5); //Tiempo de espera entre eliminación y escritura
 
     if (curFrases === frases.length - 1){ //Lógica para escribir la siguiente frase/palabra
       curFrases = 0;
