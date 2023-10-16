7a6157d0d7472c419f1197d66e039400143dc1d6
Agustín <agustinpincheira97@gmail.com>
2023-10-16 18:27:24 -0300

    llamado a API, mezcla comentarios al azar entre usuarios, renderiza todo junto

--- assets/llamadoApi.js
+++ assets/llamadoApi.js
@@ -1,55 +1,39 @@
-// // Obtén una referencia a la lista donde mostrarás los datos
-// const dataList = document.getElementById("dataList");
+// Obtén una referencia al contenedor donde mostrarás los datos
+const dataContainer = document.getElementById("dataContainer");
 
-// // URL de la API que deseas llamar
-// const apiUrl = "https://randomuser.me/api/?results=20";
+// URL de la API que deseas llamar
+const apiUrl = "https://randomuser.me/api/?results=20";
+const commentsUrl = "./assets/comentarios.json"
 
-// // Realiza una solicitud a la API utilizando fetch()
-// fetch(apiUrl)
-//     .then(response => {
-//         if (!response.ok) {
-//             throw new Error(`Error en la solicitud: ${response.status}`);
-//         }
-//         return response.json(); // Parsea la respuesta como JSON
-//     })
-//     .then(data => {
-//         // Manipula los datos de la respuesta
-//         const results = data.results;
-
-//         if (results && results.length > 0) {
-//             results.forEach(user => {
-//                 const listItem = document.createElement("li");
-//                 listItem.textContent = `Nombre: ${user.name.first} ${user.name.last}, Email: ${user.email}`;
-//                 dataList.appendChild(listItem);
-//             });
-//         } else {
-//             dataList.textContent = "No se encontraron datos.";
-//         }
-//     })
-//     .catch(error => {
-//         console.log(error)
-//     });
-
-        // Obtén una referencia al contenedor donde mostrarás los datos
-        const dataContainer = document.getElementById("dataContainer");
-
-        // URL de la API que deseas llamar
-        const apiUrl = "https://randomuser.me/api/?results=20";
-
-        // Realiza una solicitud a la API utilizando fetch()
+// Realiza una solicitud a la API de comentarios
+fetch(commentsUrl)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`Error en la solicitud de comentarios: ${response.status}`);
+        }
+        return response.json(); // Parsea los comentarios como JSON
+    })
+    .then(commentsData => {
+        // Realiza una solicitud a la API de usuarios
         fetch(apiUrl)
             .then(response => {
                 if (!response.ok) {
-                    throw new Error(`Error en la solicitud: ${response.status}`);
+                    throw new Error(`Error en la solicitud de usuarios: ${response.status}`);
                 }
-                return response.json(); // Parsea la respuesta como JSON
+                return response.json(); // Parsea los usuarios como JSON
             })
-            .then(data => {
-                // Manipula los datos de la respuesta
-                const results = data.results;
+            .then(userData => {
+                // Manipula los datos de los usuarios
+                const users = userData.results;
 
-                if (results && results.length > 0) {
-                    results.forEach(user => {
+                // Asigna comentarios al azar a los usuarios
+                const shuffledComments = shuffle(commentsData.comentarios);
+                users.forEach(user => {
+                    user.comments = [shuffledComments.pop()];
+                });
+
+                if (users && users.length > 0) {
+                    users.forEach(user => {
                         const userDiv = document.createElement("div");
                         userDiv.className = "user-card";
 
@@ -61,6 +45,14 @@
                         userName.textContent = `Nombre: ${user.name.first} ${user.name.last}`;
                         userDiv.appendChild(userName);
 
+                        const commentsList = document.createElement("ul");
+                        user.comments.forEach(comment => {
+                            const commentItem = document.createElement("li");
+                            commentItem.textContent = comment;
+                            commentsList.appendChild(commentItem);
+                        });
+
+                        userDiv.appendChild(commentsList);
                         dataContainer.appendChild(userDiv);
                     });
                 } else {
@@ -68,5 +60,22 @@
                 }
             })
             .catch(error => {
-                console.log(error)
+                console.log(error);
             });
+    })
+    .catch(error => {
+        console.log(error);
+    });
+
+// Función para mezclar (shuffle) un arreglo (acá se mezclan los comentarios)
+function shuffle(array) {
+    let currentIndex = array.length, randomIndex, temporaryValue;
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    return array;
+}
