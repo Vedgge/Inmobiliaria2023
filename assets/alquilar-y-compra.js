5cffa4d29fc0a75dc2cd45f75b5e19b0b141cbf2
Facundo <facundosavanco@gmail.com>
2023-10-26 20:18:31 -0300

    Optimizado de script para dropdown de filtros y sync con comprar

--- assets/alquilar-y-compra.js
+++ assets/alquilar-y-compra.js
@@ -1,147 +1,65 @@
 document.addEventListener('DOMContentLoaded', function () {
     // DROPDOWN FILTROS
 
+    function inicializarDropdown(buttonClass, dropdownClass) {
+        const button = document.querySelector(buttonClass);
+        const dropdown = document.querySelector(dropdownClass);
+        let timeoutDropdown;
 
-    function cerrarDropdownOperacion() {
-        filtrosDropdownOpeacion.classList.remove("active");
+        function closeDropdown() {
+            dropdown.classList.remove("active");
+        }
+
+        function openDropdown(e) {
+            clearTimeout(timeoutDropdown);
+            dropdown.classList.add("active");
+            e.stopPropagation();
+        }
+
+        function manejarSalidaMouse() {
+            timeoutDropdown = setTimeout(closeDropdown, 200);
+        }
+
+        function manejarEntradaMouse() {
+            clearTimeout(timeoutDropdown);
+            dropdown.classList.add("active");
+        }
+
+        button.addEventListener('click', openDropdown);
+        button.addEventListener('mouseleave', manejarSalidaMouse);
+        dropdown.addEventListener('mouseenter', manejarEntradaMouse);
+
+
+        document.addEventListener('click', function (e) { // Cerrar los dropdwons al hacer click en cualquier lugar fuera de ellos
+            if (!dropdown.contains(e.target) && e.target !== button) {
+                closeDropdown();
+            }
+        });
     }
 
-    // DROPDOWN TIPO DE OPERACION
+    inicializarDropdown('.alquilar_dropdown_btn', '.dropdown_tipo_operacion');
+    inicializarDropdown('.propiedad_btn', '.propiedad_dropdown');
+    inicializarDropdown('.precio_btn', '.precio_dropdown');
+    inicializarDropdown('.area_btn', '.area_dropdown');
+    // Inicializar los dropdown por cada seccion
 
-    const botonOperacion = document.querySelector('.alquilar_dropdown_btn');
-    const filtrosDropdownOpeacion = document.querySelector('.dropdown_tipo_operacion');
-    let timeoutDropdownOperacion;
+    //CERRAR DROPDOWN CON EL BOTON APILCAR
 
-    botonOperacion.addEventListener('click', function (e) {
-        clearTimeout(timeoutDropdownOperacion);
-        filtrosDropdownOpeacion.classList.add("active");
-        e.stopPropagation(); 
+    const aplicarButones = document.querySelectorAll('.aplicar_btn');
+
+    aplicarButones.forEach((button) => {
+        button.addEventListener('click', function () {
+            const dropdownSection = button.closest('.dropdown_prop_busq'); // Encontrar el elemento padre con clase "dropdown_prop_busq"
+            dropdownSection.classList.remove('active'); // Removerle la clase active
+        });
     });
-
-    botonOperacion.addEventListener('mouseleave', function () {
-        timeoutDropdownOperacion = setTimeout(cerrarDropdownOperacion, 200);
-    });
-
-    filtrosDropdownOpeacion.addEventListener('mouseenter', function () {
-        clearTimeout(timeoutDropdownOperacion);
-        filtrosDropdownOpeacion.classList.add("active");
-    });
-
-    // Cerrar dropdowns cuando se clickea en cualquier lugar que no sean el dropdown
-    document.addEventListener('click', function (e) {
-        if (!filtrosDropdownOpeacion.contains(e.target) && e.target !== botonOperacion) {
-            cerrarDropdownOperacion();
-        }
-    });
-
-    // DROPDOWN TIPO DE PROPIEDAD
-
-    const botonFiltroPropiedad = document.querySelector('.propiedad_btn');
-    const filtroDropdownPropiedad = document.querySelector('.propiedad_dropdown');
-    let timeoutDropdownPropiedad;
-    
-    function cerrarDropownFiltroPropiedad() {
-        filtroDropdownPropiedad.classList.remove("active");
-    }
-
-    botonFiltroPropiedad.addEventListener('click', function (e) {
-        clearTimeout(timeoutDropdownPropiedad);
-        filtroDropdownPropiedad.classList.add("active");
-        e.stopPropagation(); 
-    });
-
-    botonFiltroPropiedad.addEventListener('mouseleave', function () {
-        timeoutDropdownPropiedad = setTimeout(cerrarDropownFiltroPropiedad, 200);
-    });
-
-    filtroDropdownPropiedad.addEventListener('mouseenter', function () {
-        clearTimeout(timeoutDropdownPropiedad);
-        filtroDropdownPropiedad.classList.add("active");
-
-    });
-
-    // Cerrar dropdowns cuando se clickea en cualquier lugar que no sean el dropdown
-    document.addEventListener('click', function (e) {
-        if (!filtroDropdownPropiedad.contains(e.target) && e.target !== botonFiltroPropiedad) {
-            cerrarDropownFiltroPropiedad();
-        }
-    });
-
-
-    // DROPDOWN PRECIO
-
-    const botonFiltroPrecio = document.querySelector('.precio_btn');
-    const filtroDropdownPrecio = document.querySelector('.precio_dropdown');
-    let timeoutDropdownPrecio;
-    
-    function cerrarDropownFiltroPrecio() {
-        filtroDropdownPrecio.classList.remove("active");
-    }
-
-    botonFiltroPrecio.addEventListener('click', function (e) {
-        clearTimeout(timeoutDropdownPrecio);
-        filtroDropdownPrecio.classList.add("active");
-        e.stopPropagation(); 
-    });
-
-    botonFiltroPrecio.addEventListener('mouseleave', function () {
-        timeoutDropdownPrecio = setTimeout(cerrarDropownFiltroPrecio, 200);
-    });
-
-    filtroDropdownPrecio.addEventListener('mouseenter', function () {
-        clearTimeout(timeoutDropdownPrecio);
-        filtroDropdownPrecio.classList.add("active");
-
-    });
-
-    // Cerrar dropdowns cuando se clickea en cualquier lugar que no sean el dropdown
-    document.addEventListener('click', function (e) {
-        if (!filtroDropdownPrecio.contains(e.target) && e.target !== botonFiltroPrecio) {
-            cerrarDropownFiltroPrecio();
-        }
-    });
-
-
-    // DROPDOWN AREA
-
-    const botonFiltroArea = document.querySelector('.area_btn');
-    const filtroDropdownArea = document.querySelector('.area_dropdown');
-    let timeoutDropdownArea;
-    
-    function cerrarDropownFiltroArea() {
-        filtroDropdownArea.classList.remove("active");
-    }
-
-    botonFiltroArea.addEventListener('click', function (e) {
-        clearTimeout(timeoutDropdownArea);
-        filtroDropdownArea.classList.add("active");
-        e.stopPropagation(); 
-    });
-
-    botonFiltroArea.addEventListener('mouseleave', function () {
-        timeoutDropdownArea = setTimeout(cerrarDropownFiltroArea, 200);
-    });
-
-    filtroDropdownArea.addEventListener('mouseenter', function () {
-        clearTimeout(timeoutDropdownArea);
-        filtroDropdownArea.classList.add("active");
-
-    });
-
-    // Cerrar dropdowns cuando se clickea en cualquier lugar que no sean el dropdown
-    document.addEventListener('click', function (e) {
-        if (!filtroDropdownArea.contains(e.target) && e.target !== botonFiltroArea) {
-            cerrarDropownFiltroArea();
-        }
-    });
-
 
     // DROPDOWN MÁS FILTROS
 
     // const botonFiltros = document.querySelector('.filtros_btn');
     // const masFiltroDropdown = document.querySelector('.area_dropdown');
     // let timeoutDropdownArea;
-    
+
     // function cerrarDropownFiltroArea() {
     //     filtroDropdownArea.classList.remove("active");
     // }
@@ -174,7 +92,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const botonOrdenarBy = document.querySelector('.orden_btn');
     const ordenDropdown = document.querySelector('.contenedor_btn_orden');
     let timeoutDropdownOrden;
-    
+
     function cerrarDropownOrden() {
         ordenDropdown.classList.remove("active");
     }
@@ -182,7 +100,7 @@ document.addEventListener('DOMContentLoaded', function () {
     botonOrdenarBy.addEventListener('click', function (e) {
         clearTimeout(timeoutDropdownOrden);
         ordenDropdown.classList.add("active");
-        e.stopPropagation(); 
+        e.stopPropagation();
     });
 
     botonOrdenarBy.addEventListener('mouseleave', function () {
